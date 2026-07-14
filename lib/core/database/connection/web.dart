ff50ca1fdd4ed99e84296e4ad4d9b3603093b110
afakhor <31812946+afakhor@users.noreply.github.com>
2026-07-15 00:43:51 +0700

    Update web.dart

--- lib/core/database/connection/web.dart
+++ lib/core/database/connection/web.dart
@@ -1,8 +1,7 @@
-import 'package:drift/drift.dart';
-import 'package:drift/web.dart';
+import 'package:drift/drift.dart'; // PAKE drift AJA, BUKAN drift/web
 
 DatabaseConnection connect() {
   return DatabaseConnection.delayed(Future.value(
-    WebDatabase('ud_putra_web_db'),
+    WebDatabase('ud_putra_web_db'), // WebDatabase udah ada di package drift
   ));
 }
