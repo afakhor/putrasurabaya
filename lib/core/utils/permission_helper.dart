05e07d7b6b368ce6a2e9a41108435f3c1f24ced2
afakhor <31812946+afakhor@users.noreply.github.com>
2026-07-17 08:45:24 +0700

    Update permission_helper.dart

--- lib/core/utils/permission_helper.dart
+++ lib/core/utils/permission_helper.dart
@@ -1,15 +1,10 @@
 import 'package:permission_handler/permission_handler.dart';
-import 'package:flutter/foundation.dart';
 
 Future<void> requestBluetoothPermissions() async {
-  if (kIsWeb) return;
-
-  // Minta izin secara bertahap atau kolektif yang aman untuk Android baru & lama
   Map<Permission, PermissionStatus> statuses = await [
     Permission.bluetoothConnect,
     Permission.bluetoothScan,
-    //Permission.location,
   ].request();
 
-  print('Bluetooth permission status: $statuses');
+  print('Android Bluetooth permission status: $statuses');
 }
