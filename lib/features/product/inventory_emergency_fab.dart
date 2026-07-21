716c065f915fa6a18362b0ab57ac8d44ae1a0c10
afakhor <31812946+afakhor@users.noreply.github.com>
2026-07-22 00:24:25 +0700

    Update inventory_emergency_fab.dart

--- lib/features/product/inventory_emergency_fab.dart
+++ lib/features/product/inventory_emergency_fab.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:drift/drift.dart';
+import 'package:drift/drift.dart' show Value;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../core/database/local_database.dart';
 
