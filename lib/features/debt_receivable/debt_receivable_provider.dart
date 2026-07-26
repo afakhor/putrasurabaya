3c8f27bf2590e967b28d23734e1abdafe36196e3
afakhor <31812946+afakhor@users.noreply.github.com>
2026-07-26 19:52:14 +0700

    Update debt_receivable_provider.dart

--- lib/features/debt_receivable/debt_receivable_provider.dart
+++ lib/features/debt_receivable/debt_receivable_provider.dart
@@ -1,6 +1,6 @@
 // lib/features/debt_receivable/debt_receivable_provider.dart
 
-import 'package0:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../core/database/local_database.dart';
 import '../../core/database/daos/receivables_dao.dart'; // <--- IMPORT DI SINI
 
