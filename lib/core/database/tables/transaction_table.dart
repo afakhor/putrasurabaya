5c39e272ecee75335988505c1e313ce6c1916578
afakhor <31812946+afakhor@users.noreply.github.com>
2026-07-28 09:01:12 +0700

    Update transaction_table.dart

--- lib/core/database/tables/transaction_table.dart
+++ lib/core/database/tables/transaction_table.dart
@@ -10,12 +10,12 @@ class Transactions extends Table {
   TextColumn get shiftId => text().nullable()();
   TextColumn get customerId => text().nullable()();
   RealColumn get subtotal => real()();
-  RealColumn get discountTotal => real().withDefault(const Constant(0))();
-  RealColumn get taxTotal => real().withDefault(const Constant(0))();
+  RealColumn get discountTotal => real().withDefault(const Constant(0.0))();
+  RealColumn get taxTotal => real().withDefault(const Constant(0.0))();
   RealColumn get total => real()();
-  RealColumn get paid => real().withDefault(const Constant(0))();
-  RealColumn get debt => real().withDefault(const Constant(0))();
-  RealColumn get change => real().withDefault(const Constant(0))();
+  RealColumn get paid => real().withDefault(const Constant(0.0))();
+  RealColumn get debt => real().withDefault(const Constant(0.0))();
+  RealColumn get change => real().withDefault(const Constant(0.0))();
   TextColumn get paymentMethod => text().withDefault(const Constant('cash'))();
   DateTimeColumn get date => dateTime().withDefault(currentDateAndTime)();
   BoolColumn get isSynced => boolean().withDefault(const Constant(false))(); 
@@ -31,7 +31,7 @@ class TransactionItems extends Table {
   TextColumn get productId => text().references(Products, #id)(); 
   RealColumn get quantity => real()();
   RealColumn get price => real()();
-  RealColumn get buyPriceAtTransaction => real().withDefault(const Constant(0))(); 
+  RealColumn get buyPriceAtTransaction => real().withDefault(const Constant(0.0))(); 
   TextColumn get unit => text()();
 
   @override 
