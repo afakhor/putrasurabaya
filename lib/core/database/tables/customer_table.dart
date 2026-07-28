04a03b3e1a2957b38c05c028ca9e6eb040c37ba3
afakhor <31812946+afakhor@users.noreply.github.com>
2026-07-28 09:00:17 +0700

    Update customer_table.dart

--- lib/core/database/tables/customer_table.dart
+++ lib/core/database/tables/customer_table.dart
@@ -5,7 +5,7 @@ class Customers extends Table {
   TextColumn get id => text()();
   TextColumn get name => text()();
   TextColumn get phone => text().nullable()();
-  RealColumn get totalDebt => real().withDefault(const Constant(0))();
+  RealColumn get totalDebt => real().withDefault(const Constant(0.0))(); // <--- Ubah 0 jadi 0.0
 
   @override 
   Set<Column> get primaryKey => {id};
