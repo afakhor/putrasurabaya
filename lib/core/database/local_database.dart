a75bb0a87a780baeadd27ffa5d2bc0f218d4ae4d
afakhor <31812946+afakhor@users.noreply.github.com>
2026-07-28 15:17:32 +0700

    Update local_database.dart

--- lib/core/database/local_database.dart
+++ lib/core/database/local_database.dart
@@ -1,4 +1,3 @@
-local_database sebelum diperbaiki META.AI
 import 'package:drift/drift.dart';
 import 'package:drift_flutter/drift_flutter.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
