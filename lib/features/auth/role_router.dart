5fdbda38cb70cb267f784e097a90c106b7fa670b
afakhor <31812946+afakhor@users.noreply.github.com>
2026-07-31 18:27:30 +0700

    Update role_router.dart

--- lib/features/auth/role_router.dart
+++ lib/features/auth/role_router.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ud_putra_kasir/core/database/local_database.dart';
-import 'package:ud_putra_kasir/features/superuser/presentation/superuser_shell.dart';
-import 'package:ud_putra_kasir/features/admin/presentation/admin_shell.dart';
-import 'package:ud_putra_kasir/features/salesman/presentation/salesman_shell.dart';
+import 'package:ud_putra_kasir/features/aut/rooms/superuser_shell.dart';
+import 'package:ud_putra_kasir/features/auth/rooms/admin_shell.dart';
+import 'package:ud_putra_kasir/features/auth/rooms/salesman_shell.dart';
 
 Widget getHomeByRole(UserData user) {
   final role = user.role.toLowerCase();
