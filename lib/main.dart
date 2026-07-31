efcef6abd69e56dd2b1fe41324595ec2b5a117a4
afakhor <31812946+afakhor@users.noreply.github.com>
2026-07-31 10:18:50 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
-import '/../core/database/utils/config.dart'; // sesuaikan path kamu
+import '/../core/utils/config.dart'; // sesuaikan path kamu
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
