59e5b14368c9b6ffc05a9c0a4bca6fd6313ee4eb
afakhor <31812946+afakhor@users.noreply.github.com>
2026-07-31 19:29:03 +0700

    Update pos_template.dart

--- lib/core/utils/templates/pos_template.dart
+++ lib/core/utils/templates/pos_template.dart
@@ -1,48 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:ud_putra_kasir/features/pos/pos_state.dart'; // Sesuaikan path model Anda
 import '../struk_components.dart';
+import 'struk_dto.dart';
 
 class PosReceiptTemplate extends StatelessWidget {
-  final PosCartState cart;
+  final PosReceiptDTO data;
 
-  const PosReceiptTemplate({required this.cart, super.key});
+  const PosReceiptTemplate({required this.data, super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Formatter untuk mata uang
-    final currencyFormatter = NumberFormat.currency(
-      locale: 'id_ID', 
-      symbol: 'Rp ', 
-      decimalDigits: 0
-    );
-
+    final currency = NumberFormat.currency(locale: 'id_ID', symbol: 'Rp ', decimalDigits: 0);
+    
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
         const StrukHeader("UD. PUTRA SURABAYA"),
         const StrukInfoText("Jl. Surabaya - Sidoarjo"),
-        const StrukInfoText("Tgl: ${DateFormat('dd/MM/yyyy HH:mm').format(DateTime.now())}"),
+        StrukInfoText("Tgl: ${DateFormat('dd/MM/yyyy HH:mm').format(DateTime.now())}"),
         const StrukDivider(),
-        
-        // Loop Daftar Item
-        ...cart.items.map((it) => StrukRow(
+
+        ...data.items.map((it) => StrukRow(
           "${it.productName} x ${it.quantity.toStringAsFixed(0)}", 
-          currencyFormatter.format(it.subtotal)
+          currency.format(it.subtotal)
         )),
-        
+
         const StrukDivider(),
-        
-        // Ringkasan Pembayaran
-        StrukRow("TOTAL:", currencyFormatter.format(cart.total), isBold: true),
-        
-        if (cart.paymentMethod == 'cash') ...[
-          StrukRow("BAYAR:", currencyFormatter.format(cart.paidAmount)),
-          StrukRow("KEMBALI:", currencyFormatter.format(cart.change)),
-        ] else if (cart.paymentMethod == 'tempo') ...[
-          StrukRow("DP/UM:", currencyFormatter.format(cart.paidAmount)),
+        StrukRow("TOTAL:", currency.format(data.total), isBold: true),
+
+        if (data.paymentMethod == 'cash') ...[
+          StrukRow("BAYAR:", currency.format(data.paidAmount)),
+          StrukRow("KEMBALI:", currency.format(data.change)),
+        ] else if (data.paymentMethod == 'tempo') ...[
+          StrukRow("DP/UM:", currency.format(data.paidAmount)),
           const StrukStatusTag(status: "PIUTANG", isUnpaid: true),
-          StrukRow("SISA HUTANG:", currencyFormatter.format(cart.debt), isBold: true),
+          StrukRow("SISA HUTANG:", currency.format(data.debt), isBold: true),
         ],
         
         const SizedBox(height: 10),
