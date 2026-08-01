98c9c7c8718d75e2aefe1e70f3e0b06bb1dc1d84
afakhor <31812946+afakhor@users.noreply.github.com>
2026-08-01 13:37:58 +0700

    Update role_router.dart

--- lib/features/auth/role_router.dart
+++ lib/features/auth/role_router.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ud_putra_kasir/core/database/local_database.dart';
-import 'package:ud_putra_kasir/features/aut/rooms/superuser_shell.dart';
+import 'package:ud_putra_kasir/features/auth/rooms/superuser_shell.dart';
 import 'package:ud_putra_kasir/features/auth/rooms/admin_shell.dart';
 import 'package:ud_putra_kasir/features/auth/rooms/salesman_shell.dart';
 
