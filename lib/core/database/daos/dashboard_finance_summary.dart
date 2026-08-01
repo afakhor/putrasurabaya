0013260de8bf612cdcd358d33df06a1a6e857349
afakhor <31812946+afakhor@users.noreply.github.com>
2026-08-01 09:51:34 +0700

    Create dashboard_finance_summary.dart

---
+++ lib/core/database/daos/dashboard_finance_summary.dart
@@ -0,0 +1,23 @@
+class DashboardFinanceSummary {
+  final double omsetHariIni;
+  final int jumlahTransaksiHariIni;
+  final double labaKotorHariIni;
+  final double pengeluaranHariIni;
+  final double totalSisaHutang;
+  final int jumlahHutangAktif;
+  final double totalSisaPiutang;
+  final int jumlahPiutangAktif;
+  final int stokMenipisCount;
+
+  DashboardFinanceSummary({
+    required this.omsetHariIni,
+    required this.jumlahTransaksiHariIni,
+    required this.labaKotorHariIni,
+    required this.pengeluaranHariIni,
+    required this.totalSisaHutang,
+    required this.jumlahHutangAktif,
+    required this.totalSisaPiutang,
+    required this.jumlahPiutangAktif,
+    required this.stokMenipisCount,
+  });
+}
