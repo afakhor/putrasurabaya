e402b6f3ee198ce387624e8d7eacca173bee697a
afakhor <31812946+afakhor@users.noreply.github.com>
2026-08-03 12:30:37 +0700

    Update manage_users_page.dart

--- lib/features/auth/superuser/manage_users_page.dart
+++ lib/features/auth/superuser/manage_users_page.dart
@@ -32,7 +32,7 @@ class ManageUsersPage extends ConsumerWidget {
           itemBuilder: (c, i) {
             final u = list[i];
             return Container(
-              margin: const EdgeInsets.only(bottom: 70),
+              margin: const EdgeInsets.only(bottom: 90),
               decoration: BoxDecoration(
                 color: Colors.white.withOpacity(0.92),
                 borderRadius: BorderRadius.circular(14),
