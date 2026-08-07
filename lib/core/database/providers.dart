be8f566c9806cda7120450efe8a8663966a56f87
afakhor <31812946+afakhor@users.noreply.github.com>
2026-08-07 19:26:39 +0700

    Update providers.dart

--- lib/core/database/providers.dart
+++ lib/core/database/providers.dart
@@ -1,14 +1,23 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'local_database.dart';
+import 'daos/stock_mutation_dao.dart';
+
+final productDaoProvider = Provider((ref) => ref.watch(localDatabaseProvider).productDao);
+final stockMutationDaoProvider = Provider((ref) => ref.watch(localDatabaseProvider).stockMutationDao);
+final dashboardDaoProvider = Provider((ref) => ref.watch(localDatabaseProvider).dashboardDao);
 
 final productsStreamProvider = StreamProvider<List<ProductData>>((ref) {
   return ref.watch(localDatabaseProvider).productDao.watchAllProducts();
 });
-
 final productStreamProvider = StreamProvider.family<ProductData?, String>((ref, id) {
   return ref.watch(localDatabaseProvider).productDao.watchProductById(id);
 });
-
-final allStockMutationsStreamProvider = StreamProvider.family<List<StockCardItemData>, dynamic>((ref, filter) {
-  return ref.watch(localDatabaseProvider).stockMutationDao.watchAllStockMutations();
+final kartuStokStreamProvider = StreamProvider.family<List<StockCardItemData>, String>((ref, productId) {
+  return ref.watch(localDatabaseProvider).stockMutationDao.watchKartuStok(productId);
+});
+final allStockMutationsStreamProvider = StreamProvider.family<List<StockCardItemData>, Map<String,dynamic>>((ref, filter) {
+  return ref.watch(localDatabaseProvider).stockMutationDao.watchAllStockMutations(
+    mutationTypeFilter: filter['type'] as String?,
+    keyword: filter['keyword'] as String?,
+  );
 });
