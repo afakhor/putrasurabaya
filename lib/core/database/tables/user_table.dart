cd46eb6588a21f49d2793da9d2aa55ac7e96dd7e
afakhor <31812946+afakhor@users.noreply.github.com>
2026-08-11 21:38:12 +0700

    Update user_table.dart

--- lib/core/database/tables/user_table.dart
+++ lib/core/database/tables/user_table.dart
@@ -1,16 +1,15 @@
 import 'package:drift/drift.dart';
-
 @DataClassName('UserData')
 class Users extends Table {
   TextColumn get id => text()();
   TextColumn get username => text().nullable()();
   TextColumn get name => text()();
-  TextColumn get role => text()(); // owner, admin, kasir, salesman
+  TextColumn get role => text()();
   TextColumn get passwordHash => text().nullable()();
   TextColumn get pinCode => text().nullable()();
   TextColumn get apiKey => text().nullable()();
   TextColumn get phone => text().nullable()();
-  TextColumn get waNumber => text().nullable()(); // untuk Auto WA Struk
+  TextColumn get waNumber => text().nullable()();
   TextColumn get salesArea => text().nullable()();
   TextColumn get address => text().nullable()();
   TextColumn get status => text().withDefault(const Constant('aktif'))();
@@ -22,18 +21,15 @@ class Users extends Table {
   BoolColumn get canProcessReturn => boolean().withDefault(const Constant(false))();
   BoolColumn get canVoidTransaction => boolean().withDefault(const Constant(false))();
   BoolColumn get canManageStock => boolean().withDefault(const Constant(false))();
-  // TAMBAHAN KEAMANAN X
   BoolColumn get canDeleteTransaction => boolean().withDefault(const Constant(false))();
   BoolColumn get canEditHpp => boolean().withDefault(const Constant(false))();
   BoolColumn get canViewProfit => boolean().withDefault(const Constant(false))();
   BoolColumn get canViewDashboardBos => boolean().withDefault(const Constant(false))();
-  
   BoolColumn get isSynced => boolean().withDefault(const Constant(false))();
   DateTimeColumn get createdAt => dateTime().withDefault(currentDateAndTime)();
   DateTimeColumn get updatedAt => dateTime().withDefault(currentDateAndTime)();
   @override Set<Column> get primaryKey => {id};
 }
-
 @DataClassName('ShiftKasirData')
 class ShiftKasir extends Table {
   TextColumn get id => text()();
@@ -53,20 +49,4 @@ class ShiftKasir extends Table {
   DateTimeColumn get createdAt => dateTime().withDefault(currentDateAndTime)();
   DateTimeColumn get updatedAt => dateTime().withDefault(currentDateAndTime)();
   @override Set<Column> get primaryKey => {id};
-}
-
-abstract class UserRole {
-  static const String superuser = 'superuser';
-  static const String owner = 'owner';
-  static const String admin = 'admin';
-  static const String kasir = 'kasir';
-  static const String salesman = 'salesman';
-}
-abstract class UserStatus {
-  static const String aktif = 'aktif';
-  static const String suspended = 'suspended';
-}
-abstract class ShiftStatus {
-  static const String open = 'open';
-  static const String closed = 'closed';
 }
